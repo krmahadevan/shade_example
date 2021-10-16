a67d5fa38d9f8aff4801f2a69277572e2acf234b
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2021-10-16 13:54:19 +0530

    Get the shadow plugin to work

--- m2/src/test/java/com/rationaleemotions/ExampleTest.java
+++ m2/src/test/java/com/rationaleemotions/ExampleTest.java
@@ -1,11 +1,13 @@
 package com.rationaleemotions;
 
 import org.testng.annotations.Test;
+import rationale.emotions.org.assertj.core.api.Assertions;
 
 public class ExampleTest {
 
   @Test
   public void testMethod() {
     System.err.println("Hello world");
+    Assertions.assertThat("").isEmpty();
   }
 }
