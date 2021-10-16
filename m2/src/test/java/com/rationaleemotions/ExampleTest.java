c15b96115f0b15b5ef969730595a8f5c66ce8a4a
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2021-10-16 11:33:26 +0530

    Initial commit

---
+++ m2/src/test/java/com/rationaleemotions/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.rationaleemotions;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+
+  @Test
+  public void testMethod() {
+    System.err.println("Hello world");
+  }
+}
